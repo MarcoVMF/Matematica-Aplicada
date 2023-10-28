d4f30e59f0e5324e3e06cd8189e80cbc31b5970e
Marco Vinicius de Melo Faria <marco.melo-faria@unesp.br>
2023-10-28 13:47:14 +0000

    atividade 5

--- Atividade 5/Atividade/exA.py
+++ Atividade 5/Atividade/exA.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def euler_modificado(f, t0, y0, h, num_steps):
     t = t0
     y = y0
@@ -50,23 +51,23 @@ def metodo_de_taylor_ordem_2(f, t0, y0, h, num_steps):
     t = t0
     y = y0
     results = {'X': [t0], 'Metodo de Taylor Ordem 2': [y0], 'Exata': [exact_solution(t0)], 'Erro Local': [0]}
-    
+
     for _ in range(num_steps):
         # Usando a expansão de Taylor de segunda ordem
-        y_new = y + h * f(t, y) + (h**2 / 2) * (f(t, y) + h * f(t, y))  # Expandindo até a segunda ordem
-        
+        y_new = y + h * f(t, y) + (h ** 2 / 2) * (f(t, y) + h * f(t, y))  # Expandindo até a segunda ordem
+
         t = t + h
-        
+
         exact_y = exact_solution(t)  # Valor exato da solução
         erro_local = (y - exact_y)  # Cálculo do erro local
-        
+
         results['X'].append(t)
         results['Metodo de Taylor Ordem 2'].append(y_new)
         results['Exata'].append(exact_y)
         results['Erro Local'].append(erro_local)
-        
+
         y = y_new
-    
+
     return pd.DataFrame(results)
 
 
@@ -74,9 +75,11 @@ def metodo_de_taylor_ordem_2(f, t0, y0, h, num_steps):
 def exact_solution(t):
     return np.exp(-t) + t + 1
 
+
 # Função que representa a derivada dy/dx = x - y + 2
 def f(t, y):
     return t - y + 2
+
 
 # Condições iniciais
 t0 = 0
@@ -92,10 +95,16 @@ num_steps = num_subintervals
 df_taylor = metodo_de_taylor_ordem_2(f, t0, y0, h, num_steps)
 h = 0.1
 n = 10
-df_euler_modificado =  euler_modificado(f, t0, y0, h, n)
-df_euler_aprimorado =  euler_aprimorado(f, t0, y0, h, n)
+df_euler_modificado = euler_modificado(f, t0, y0, h, n)
+df_euler_aprimorado = euler_aprimorado(f, t0, y0, h, n)
 
 # Exibindo o DataFrame com os valores e o erro local
+
+with pd.ExcelWriter('exercicioA.xlsx') as writer:
+    df_taylor.to_excel(writer, sheet_name='Taylor', index=False)
+    df_euler_modificado.to_excel(writer, sheet_name='Runge Modificado', index=False)
+    df_euler_aprimorado.to_excel(writer, sheet_name='Euler Aprimorado', index=False)
+
 
 # Plotando os resultados
 plt.figure(figsize=(10, 6))
@@ -108,4 +117,5 @@ plt.ylabel('y')
 plt.legend()
 plt.title("Solução da EDO usando o Método de Taylor e Euler's")
 plt.grid(True)
+plt.savefig('exercicioA.png')
 plt.show()
--- Atividade 5/Atividade/exB.py
+++ Atividade 5/Atividade/exB.py
@@ -2,17 +2,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def metodo_de_taylor_ordem_1(f, t0, y0, h, num_steps):
     t = t0
     y = y0
     results = {'X': [t0], 'Metodo de Taylor Ordem 1': [y0], 'Exata': [exact_solution(t0)], 'Erro Local': [0]}
-    
+
     for _ in range(num_steps):
         # Usando a expansão de Taylor de primeira ordem
         y_new = y + h * f(t, y)
-        
+
         t = t + h
-        
+
         exact_y = exact_solution(t)  # Valor exato da solução
         erro_local = (y - exact_y)  # Cálculo do erro local
 
@@ -20,42 +21,45 @@ def metodo_de_taylor_ordem_1(f, t0, y0, h, num_steps):
         results['Metodo de Taylor Ordem 1'].append(y_new)
         results['Exata'].append(exact_y)
         results['Erro Local'].append(erro_local)
-        
+
         y = y_new
-    
+
     return pd.DataFrame(results)
+
 
 def metodo_de_taylor_ordem_2(f, t0, y0, h, num_steps):
     t = t0
     y = y0
     results = {'X': [t0], 'Metodo de Taylor Ordem 2': [y0], 'Exata': [exact_solution(t0)], 'Erro Local': [0]}
-    
+
     for _ in range(num_steps):
         # Usando a expansão de Taylor de segunda ordem
-        y_new = y + h * f(t, y) + (h**2 / 2) * (f(t, y) + h * f(t, y))  # Expandindo até a segunda ordem
-        
+        y_new = y + h * f(t, y) + (h ** 2 / 2) * (f(t, y) + h * f(t, y))  # Expandindo até a segunda ordem
+
         t = t + h
-        
+
         exact_y = exact_solution(t)  # Valor exato da solução
         erro_local = (y - exact_y)  # Cálculo do erro local
-        
+
         results['X'].append(t)
         results['Metodo de Taylor Ordem 2'].append(y_new)
         results['Exata'].append(exact_y)
         results['Erro Local'].append(erro_local)
-        
+
         y = y_new
-    
+
     return pd.DataFrame(results)
 
 
 # Solução exata
 def exact_solution(t):
-    return np.exp(5*t) + 0.2
+    return np.exp(5 * t) + 0.2
+
 
 # Função que representa a derivada dy/dx = x - y + 2
 def f(t, y):
-    return 5*y - 1
+    return 5 * y - 1
+
 
 # Condições iniciais
 t0 = 0
@@ -67,9 +71,10 @@ num_steps = 20
 df_taylor1 = metodo_de_taylor_ordem_1(f, t0, y0, h, num_steps)
 df_taylor2 = metodo_de_taylor_ordem_2(f, t0, y0, h, num_steps)
 
-# Exibindo o DataFrame com os valores e o erro local
-print(df_taylor1)
-print(df_taylor2)
+
+with pd.ExcelWriter('exercicioB.xlsx') as writer:
+    df_taylor1.to_excel(writer, sheet_name='Taylor 1', index=False)
+    df_taylor2.to_excel(writer, sheet_name='Taylor 2', index=False)
 
 # Plotando os resultados
 plt.figure(figsize=(10, 6))
@@ -81,4 +86,5 @@ plt.ylabel('y')
 plt.legend()
 plt.title("Solução da EDO usando o Método de Taylor e Euler's")
 plt.grid(True)
+plt.savefig('exercicioB.png')
 plt.show()
